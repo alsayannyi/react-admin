711fa16e653af4edf668dfd5863fe6740bfbb2dd
Alex <al.sayannyi@gmail.com>
2019-06-06 12:34:02 +0300

    first commit with initial components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>Hi All,</p>
+    <p>
+      This app-sample has been created to test React,Redux, React Router and
+      other libraries by coding
+    </p>
+  </div>
+);
+
+export default AboutPage;
